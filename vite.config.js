37c7dce6418902fb0597a02a7e2efe1e9b96e356
FilipStefanovski1 <fikokapo031@gmail.com>
2026-02-18 03:40:23 +0100

    Build all pages (multi-page Vite)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  server: {
-    port: 5173,
-    open: true
-  }
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        agenda: resolve(__dirname, "agenda.html"),
+        competition: resolve(__dirname, "competition.html"),
+        contact: resolve(__dirname, "contact.html"),
+        expo: resolve(__dirname, "expo.html"),
+        partners: resolve(__dirname, "partners.html"),
+        speakers: resolve(__dirname, "speakers.html"),
+        tickets: resolve(__dirname, "tickets.html"),
+      },
+    },
+  },
 });
